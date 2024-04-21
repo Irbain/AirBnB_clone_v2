0d091b8b0687c1c83f66474093276bdfc200b1ce
amineirb <contact@irbaine.com>
2024-04-21 15:20:17 -0700

    Done

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""Start web application with two routings
-"""
 
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states')
 def states_list():
-    """Render template with states
-    """
     path = '8-cities_by_states.html'
     states = storage.all(State)
 
